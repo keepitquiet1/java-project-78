db643e5b51802370a09315ead3831ac97cde0dc5
David <david.klesch@gmail.com>
2023-03-28 21:03:10 +0300

    Almost done?

--- app/src/main/java/hexlet/code/schemas/StringSchema.java
+++ app/src/main/java/hexlet/code/schemas/StringSchema.java
@@ -20,6 +20,12 @@ public class StringSchema extends BaseSchema {
 
     }
 
+    public final StringSchema minLength(int minLength) {
+        Predicate<String> minLeng = x -> x.length() >= minLength;
+        addCheck(minLeng);
+        return this;
+    }
+
     public final StringSchema contains(String substring) {
         if (!substring.equals("")) {
             Predicate<String> contains = x -> x.contains(substring);
