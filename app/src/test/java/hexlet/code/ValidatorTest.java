a6912651ad2b0cfcf4bf4118fca14ad596b45767
David <david.klesch@gmail.com>
2023-03-28 20:57:11 +0300

    Almost done?

--- app/src/test/java/hexlet/code/ValidatorTest.java
+++ app/src/test/java/hexlet/code/ValidatorTest.java
@@ -15,52 +15,52 @@ class ValidatorTest {
     @Test
     void stringIsValid() {
         Validator v = new Validator();
-        StringSchema stringSchema = v.string();
+        StringSchema schema = v.string();
 
-        Assertions.assertTrue(stringSchema.isValid(""));
-        Assertions.assertTrue(stringSchema.isValid(null));
+        Assertions.assertTrue(schema.isValid(""));
+        Assertions.assertTrue(schema.isValid(null));
 
-        stringSchema.required();
+        schema.required();
 
-        Assertions.assertTrue(stringSchema.isValid("what does the fox say"));
-        Assertions.assertTrue(stringSchema.isValid("hexlet"));
+        Assertions.assertTrue(schema.isValid("what does the fox say"));
+        Assertions.assertTrue(schema.isValid("hexlet"));
 
-        Assertions.assertFalse(stringSchema.isValid(null));
-        Assertions.assertFalse(stringSchema.isValid(5));
-        Assertions.assertFalse(stringSchema.isValid(""));
+        Assertions.assertFalse(schema.isValid(null));
+        Assertions.assertFalse(schema.isValid(5));
+        Assertions.assertFalse(schema.isValid(""));
 
-        Assertions.assertTrue(stringSchema.contains("wh").isValid("what does the fox say"));
-        Assertions.assertTrue(stringSchema.contains("what").isValid("what does the fox say"));
+        Assertions.assertTrue(schema.contains("wh").isValid("what does the fox say"));
+        Assertions.assertTrue(schema.contains("what").isValid("what does the fox say"));
 
-        Assertions.assertFalse(stringSchema.contains("whatthe").isValid("what does the fox say"));
-        Assertions.assertFalse(stringSchema.isValid("what does the fox say"));
+        Assertions.assertFalse(schema.contains("whatthe").isValid("what does the fox say"));
+        Assertions.assertFalse(schema.isValid("what does the fox say"));
 
     }
 
     @Test
     void numberIsValid() {
         Validator v = new Validator();
-        NumberSchema numberSchema = v.number();
+        NumberSchema schema = v.number();
 
 // Пока не вызван метод required(), null считается валидным
-        Assertions.assertTrue(numberSchema.isValid(null)); // true
-        Assertions.assertTrue(numberSchema.positive().isValid(null)); // true
+        Assertions.assertTrue(schema.isValid(null)); // true
+        Assertions.assertTrue(schema.positive().isValid(null)); // true
 
-        numberSchema.required();
+        schema.required();
 
-        Assertions.assertFalse(numberSchema.isValid(null)); // false
-        Assertions.assertTrue(numberSchema.isValid(10)); // true
-        Assertions.assertFalse(numberSchema.isValid("5")); // false
-        Assertions.assertFalse(numberSchema.isValid(-10)); // false
+        Assertions.assertFalse(schema.isValid(null)); // false
+        Assertions.assertTrue(schema.isValid(10)); // true
+        Assertions.assertFalse(schema.isValid("5")); // false
+        Assertions.assertFalse(schema.isValid(-10)); // false
 //  Ноль - не положительное число
-        Assertions.assertFalse(numberSchema.isValid(0)); // false
+        Assertions.assertFalse(schema.isValid(0)); // false
 
-        Assertions.assertFalse(numberSchema.range(5, 10).isValid(11)); //false
+        Assertions.assertFalse(schema.range(5, 10).isValid(11)); //false
 
-        Assertions.assertTrue(numberSchema.isValid(5)); // true
-        Assertions.assertTrue(numberSchema.isValid(10)); // true
-        Assertions.assertFalse(numberSchema.isValid(4)); // false
-        Assertions.assertFalse(numberSchema.isValid(11)); // false
+        Assertions.assertTrue(schema.isValid(5)); // true
+        Assertions.assertTrue(schema.isValid(10)); // true
+        Assertions.assertFalse(schema.isValid(4)); // false
+        Assertions.assertFalse(schema.isValid(11)); // false
     }
 
     @Test
